f62eac92841cac57032eb9245e1e483541854696
snxq <snxq1995@gmail.com>
2020-11-19 12:35:31 +0800

    :green_heart: fix: handle http.write error

--- other/go/multipart-form/server.go
+++ other/go/multipart-form/server.go
@@ -8,15 +8,17 @@ import (
 
 // Server multipart/form server
 type Server struct {
+	w http.ResponseWriter
 }
 
 // ServeHTTP impl http.Handler
 // simple upload server. return file content.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.w = w
 	fmt.Println(r.URL, r.RemoteAddr)
 	// max memory size 64M
 	if err := r.ParseMultipartForm(64 << 20); err != nil {
-		w.Write([]byte(fmt.Sprintf("parse multipart/form data failed: %+v", err)))
+		s.w.Write([]byte(fmt.Sprintf("parse multipart/form data failed: %+v", err)))
 		return
 	}
 	defer r.MultipartForm.RemoveAll()
@@ -25,16 +27,25 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		for _, f := range fs {
 			file, err := f.Open()
 			if err != nil {
-				w.Write([]byte(fmt.Sprintf("open file '%s' failed: %+v", f.Filename, err)))
+				s.w.Write([]byte(fmt.Sprintf("open file '%s' failed: %+v", f.Filename, err)))
 				return
 			}
 			b, err := ioutil.ReadAll(file)
 			if err != nil {
-				w.Write([]byte(fmt.Sprintf("read file '%s' failed: %+v", f.Filename, err)))
+				s.w.Write([]byte(fmt.Sprintf("read file '%s' failed: %+v", f.Filename, err)))
+				return
 			}
-			w.Write([]byte(f.Filename + ":"))
-			w.Write(b)
+			s.w.Write([]byte(f.Filename + ":"))
+			s.w.Write(b)
 		}
 	}
 	return
+}
+
+func (s *Server) Write(p []byte) (n int, err error) {
+	n, err = s.w.Write(p)
+	if err != nil {
+		fmt.Printf("Write response failed: %+v", err)
+	}
+	return n, err
 }
